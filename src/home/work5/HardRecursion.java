f6a6b4a855e2cafb364e8c5d8f56fde4418c52e9
Stanislav <staspusuliak@gmail.com>
2021-10-08 15:50:11 +0300

    Home work 6 Added class HardRecursion Home Work 6 has already done!

--- src/home/work5/HardRecursion.java
+++ src/home/work5/HardRecursion.java
@@ -1,34 +1,44 @@
-//package home.work5;
-//
-//import javax.swing.*;
-//
-//public class HardRecursion {
-//
-//    private static boolean status = true;
-//
-//    public static void main(String[] args) {
-//        int number = 13;
-//        int ourNumber = 1;
-//        System.out.println(result(number));
-//    }
-//
-//    private static String result(int number){
-//
-//    }
-//
-//    private static String rec3(int number, int ourNumber) {
-//        if (ourNumber == number) return 1 + rec3(number, ourNumber);
-//
-//        if (ourNumber < number) rec5(number,ourNumber);
-//
-//    }
-//
-//    private static String rec5(int number, int ourNumber) {
-//        if (number == ourNumber) return rec5(number,ourNumber);
-//        if (ourNumber < number) {
-//            ourNumber += 5;
-//            return "+5" + rec5(number,ourNumber);
-//        }
-//        return "";
-//    }
-//}
+package home.work5;
+
+import java.util.Scanner;
+
+public class HardRecursion {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number: ");
+        int number = correctNumber();
+
+        if (result(number) != null) System.out.println(result(number));
+        else System.out.println("Solution is not found");
+    }
+
+    private static String result(int number) {
+        find(1, "1", number);
+        return find(1, "1", number);
+    }
+
+    private static String find(int current, String history, int number) {
+        if (current == number) return history;
+        else if (current > number) return null;
+        else {
+            if (find(current + 5, "(" + history + " + 5)", number) != null)
+                return find(current + 5, "(" + history + " + 5)", number);
+            if (find(current * 3, "(" + history + " * 3)", number) != null)
+                return find(current * 3, "(" + history + " * 3)", number);
+        }
+        return null;
+    }
+
+    private static int correctNumber() {                            //check the entered number
+        Scanner scanner = new Scanner(System.in);
+        int number = 0;
+        if (scanner.hasNextInt()) number = scanner.nextInt();
+        else {
+            System.out.println("Wrong argument.Please enter a number");
+            System.exit(0);
+        }
+        return number;
+    }
+
+}
