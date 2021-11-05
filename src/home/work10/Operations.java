b75cfdfdf21575f804e1374ddf778a0d3a97a62f
Stanislav <staspusuliak@gmail.com>
2021-11-05 13:16:23 +0300

    Under development

---
+++ src/home/work10/Operations.java
@@ -0,0 +1,12 @@
+package home.work10;
+
+public class Operations {
+
+    public int[] addition(Fraction num1, Fraction num2) {
+        return
+    }
+
+    public static int[] subtraction(Fraction num1, Fraction num2)
+
+
+}
