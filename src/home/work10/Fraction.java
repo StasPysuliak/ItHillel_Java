0ca0733bf3ab77ed04751be66dd86f7a70b76dfe
Stanislav <staspusuliak@gmail.com>
2021-11-08 20:02:51 +0300

    In process

--- src/home/work10/Fraction.java
+++ src/home/work10/Fraction.java
@@ -10,9 +10,66 @@ public class Fraction {
         this.intPart = intPart;
         this.decimalPart = decimalPart;
     }
+
     public Fraction(String str) {
         this.str = str;
     }
 
+    static int[] addition(Fraction num1, Fraction num2) {
+
+        int num1decimalPart = num1.decimalPart;
+        int num2decimalPart = num2.decimalPart;
+        int num1Length = length(num1.decimalPart);
+        int num2Length = length(num2.decimalPart);
+        int intPartRes = num1.intPart + num2.intPart;
+        int decimalPartRes;
+        int tmp;
+
+        if (num1Length < num2Length) tmp = exponentiation(num1decimalPart, num2Length - num1Length);
+        else tmp = exponentiation(num2decimalPart, num1Length - num2Length);
+        decimalPartRes = tmp + num2decimalPart;
+
+        if (length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) {
+            decimalPartRes -= exponentiation(1, length(decimalPartRes));
+            intPartRes++;
+        }
+        return new int[]{intPartRes, decimalPartRes};
+    }
+
+
+    static int[] subtraction(Fraction num1, Fraction num2) {
+        int num1decimalPart = num1.decimalPart;
+        int num2decimalPart = num2.decimalPart;
+        int num1Length = length(num1.decimalPart);
+        int num2Length = length(num2.decimalPart);
+        int intPartRes = num1.intPart - num2.intPart;
+        int decimalPartRes;
+        int tmp;
+
+        if (num1Length < num2Length) tmp = exponentiation(num1decimalPart, num2Length - num1Length);
+        else tmp = exponentiation(num2decimalPart, num1Length - num2Length);
+        decimalPartRes = tmp - num2decimalPart;
+
+        if (length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) {
+            decimalPartRes -= exponentiation(1, length(decimalPartRes));
+            intPartRes--;
+        }
+        return new int[]{intPartRes, decimalPartRes};
+    }
+
+    private static int length(int num) {
+        return num < 10 ? 0 : length(num / 10) + 1;
+    }
+
+    private static int exponentiation(int num, int count) {
+        return count == 0 ? num : exponentiation(num, count - 1) * 10;
+    }
+
+    static boolean validationArguments(String str) {
+        if (Integer.parseInt(str) > Short.MAX_VALUE || Integer.parseInt(str) < 0) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/home/work10/Main.java
+++ src/home/work10/Main.java
@@ -1,7 +1,32 @@
 package home.work10;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String[] data = str.split(" ");
+        String[] str1 = data[0].split(",");
+        String[] str2 = data[2].split(",");
 
+//        if (!Fraction.validationArguments(str1[1]) && !Fraction.validationArguments(str2[1])){
+//            System.out.println("Incorrect arguments");
+//            System.exit(0);
+//        }
+
+        Fraction fraction1 = new Fraction(Integer.parseInt(str1[0]), Short.parseShort(str1[1]));
+        Fraction fraction2 = new Fraction(Integer.parseInt(str2[0]), Short.parseShort(str2[1]));
+        switch (data[1]) {
+            case "plus":
+                System.out.println(Arrays.toString(Fraction.addition(fraction1, fraction2)));
+                break;
+            case "minus":
+                System.out.println(Arrays.toString(Fraction.subtraction(fraction1, fraction2)));
+                break;
+            default:
+                System.out.println("Non-existent function");
+        }
     }
 }
