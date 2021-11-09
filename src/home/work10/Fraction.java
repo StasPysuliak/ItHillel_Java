4a8ec582a0ab3d081a3ff99278b90bd611b42bda
Stanislav <staspusuliak@gmail.com>
2021-11-09 10:25:20 +0200

    In process

--- src/home/work10/Fraction.java
+++ src/home/work10/Fraction.java
@@ -1,5 +1,7 @@
 package home.work10;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Fraction {
 
     private int intPart;
@@ -16,27 +18,37 @@ public class Fraction {
     }
 
     static int[] addition(Fraction num1, Fraction num2) {
-
         int num1decimalPart = num1.decimalPart;
         int num2decimalPart = num2.decimalPart;
         int num1Length = length(num1.decimalPart);
         int num2Length = length(num2.decimalPart);
         int intPartRes = num1.intPart + num2.intPart;
         int decimalPartRes;
-        int tmp;
+
+        if (num1.intPart < 0) num1decimalPart *= -1;
+        if (num2.intPart < 0) num2decimalPart *= -1;
 
         if (num1Length < num2Length) {
-            tmp = exponentiation(num1decimalPart, num2Length - num1Length);
-            decimalPartRes = tmp + num2decimalPart;
+            num1decimalPart = exponentiation(num1decimalPart, num2Length - num1Length);             //alignment of numbers
+            num1Length = length(num1decimalPart);
+            decimalPartRes = num1decimalPart + num2decimalPart;
         } else {
-            tmp = exponentiation(num2decimalPart, num1Length - num2Length);
-            decimalPartRes = tmp + num1decimalPart;
+            num2decimalPart = exponentiation(num2decimalPart, num1Length - num2Length);             //alignment of numbers
+            num2Length = length(num2decimalPart);
+            decimalPartRes = num2decimalPart + num1decimalPart;
         }
 
-        if (length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) {
+        if ((length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) && decimalPartRes < 0) {
+            decimalPartRes += exponentiation(1, length(decimalPartRes));
+            intPartRes--;
+        }
+        if ((length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) && decimalPartRes > 0) {       //increase intPartRes by one
             decimalPartRes -= exponentiation(1, length(decimalPartRes));
             intPartRes++;
         }
+
+
+
         return new int[]{intPartRes, decimalPartRes};
     }
 
@@ -48,24 +60,36 @@ public class Fraction {
         int num2Length = length(num2.decimalPart);
         int intPartRes = num1.intPart - num2.intPart;
         int decimalPartRes;
-        int tmp;
+
+        if (num1.intPart < 0) num1decimalPart *= -1;
+        if (num2.intPart < 0) num2decimalPart *= -1;
 
         if (num1Length < num2Length) {
-            tmp = exponentiation(num1decimalPart, num2Length - num1Length);
-            decimalPartRes = tmp - num2decimalPart;
+            num1decimalPart = exponentiation(num1decimalPart, num2Length - num1Length);
+
+            decimalPartRes = num1decimalPart - num2decimalPart;
         } else {
-            tmp = exponentiation(num2decimalPart, num1Length - num2Length);
-            decimalPartRes = tmp - num1decimalPart;
+            num2decimalPart = exponentiation(num2decimalPart, num1Length - num2Length);
+
+            decimalPartRes = num2decimalPart - num1decimalPart;
         }
 
+        if ((length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) && decimalPartRes < 0) {
+            decimalPartRes -= exponentiation(1, length(decimalPartRes));
+            intPartRes--;
+        }
         if (length(decimalPartRes) > num1Length || length(decimalPartRes) > num2Length) {
             decimalPartRes -= exponentiation(1, length(decimalPartRes));
             intPartRes--;
         }
+
+
+
         return new int[]{intPartRes, decimalPartRes};
     }
 
     private static int length(int num) {
+        if (num < 0) num*=-1;
         return num < 10 ? 0 : length(num / 10) + 1;
     }
 
