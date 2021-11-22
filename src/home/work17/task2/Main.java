c2b3952973d7c487f394ff972c9601c608b0354f
Stanislav <staspusuliak@gmail.com>
2021-11-22 17:53:07 +0200

    Home Work 17

---
+++ src/home/work17/task2/Main.java
@@ -0,0 +1,9 @@
+package home.work17.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        NumGenerator numGenerator = new NumGenerator(10, 10, 25);
+        System.out.println("List: " + numGenerator.generateList());
+        System.out.println("Set:  " + numGenerator.generateSet());
+    }
+}
